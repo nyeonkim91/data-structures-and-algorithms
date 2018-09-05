dc48899d020df4d52e1aefe1746e59e21e7a47e4
allstay <kim@allstayui-MacBook-Pro.local>
2018-09-05 20:31:35 +0900

    solve two-sum

--- algorithms/twoSum.js
+++ algorithms/twoSum.js
@@ -5,8 +5,43 @@
 // 1. can be done in O(n^2) time complexity
 // 2. can be done in O(n) time complexity --> 모르겠으면 harmlessRansomNote 참고
 
+// returns every pair of numbers from 'numArray' that adds up to the 'sum'
 function twoSum(numArray, sum) {
-  // returns every pair of numbers from 'numArray' that adds up to the 'sum'
+  let res = [];
+  let hash = {};
+
+  numArray.forEach(num => {
+    let counterpart = sum - num;
+    // if(counterpart in hash)
+    if(!hash[counterpart]) hash[num] = 1;
+    else {
+      for(let i = 0; i < hash[counterpart]; i++) {
+        res.push([counterpart, num]);
+      }
+      if(!hash[num]) hash[num] = 0;
+      hash[num]++;
+    }
+  })
+
+  return res;
 }
 
-// twoSum([1, 6, 4, 5, 3, 3], 7); // [[6, 1], [3, 4], [3, 4]]
+// udemy 수업 답인데 오류있음
+// function twoSum(numArray, sum) {
+//   var pairs = [];
+//   var hashTable = [];
+//
+//   for (var i = 0; i < numArray.length; i++) {
+//     var currNum = numArray[i];
+//     var counterpart = sum - currNum;
+//     if (hashTable.indexOf(counterpart) !== -1) {
+//       pairs.push([ currNum, counterpart ]);
+//     }
+//     hashTable.push(currNum);
+//   }
+//
+//   return pairs;
+// }
+
+twoSum([1, 6, 4, 5, 3, 3], 7); // [[6, 1], [3, 4], [3, 4]]
+twoSum([1, 6, 6, 6, 1, 1], 7); // [1, 6], [1, 6], [1, 6], [6, 1], [6, 1], [6, 1]
