4b8b76496a14b62ee63c3f30929a58100bb01a12
allstay <kim@allstayui-MacBook-Pro.local>
2018-09-06 14:03:37 +0900

    solve fibonacci and memoized-fibonacci
    - try memoized-fibonacci more

--- algorithms/fibonacci.js
+++ algorithms/fibonacci.js
@@ -5,7 +5,8 @@
 // 2. base case deals with the fact that the first two numbers are always 1 and 1
 
 function fibonacci(position) {
-
+  if(position < 3) return 1
+  else return fibonacci(position - 1) + fibonacci(position - 2)
 }
 
 fibonacci(4); // returns 3
--- algorithms/memoizedFibonacci.js
+++ algorithms/memoizedFibonacci.js
@@ -4,10 +4,21 @@
 // - If number is not in cache, calculate it and put it in cache so it can be used multiple times in future
 // Runtime: O(n) - linear
 
-function memoizedFibonacci(index, cache) {
-  // index: index of number in fibonacci sequence
-  // cache: an array used as memory
+// index: index of number in fibonacci sequence
+// cache: an array used as memory
+function fibMemo(index, cache) {
+  cache = cache || [];
+
+  if(index < 3) return 1
+  else if(cache[index]) return chache[index]
+  else {
+    // if(index < 3) return 1
+    cache[index] = fibMemo(index - 1) + fibMemo(index - 2);
+  }
+
+  return cache[index];
 }
+
 
 fibonacci(4); // returns 3
 fibonacci(9); // returns 34
